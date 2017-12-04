cf8568a63b1e7d03df1a7e0846348b52011f436d
ShivamNegi <shivamnegi2019@gmail.com>
2017-12-04 16:02:12 +0530

    Add get_user, send_msg, spam functions

---
+++ re_ply.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+url = "https://web.whatsapp.com/"
+
+class Whatsapp_access:
+	def __init__(self):
+		self.driver = webdriver.Firefox(executable_path="/home/bogo/Downloads/geckodriver")
+		self.driver.get(url)
+
+	def get_user(self, name):
+		try:
+			ele = self.driver.find_element_by_xpath("//span[@title='" + name + "']")
+			ele.click()
+		except:
+			print("The contact does not exist.")
+
+	def send_msg(self, msg):
+		try:
+			ele = self.driver.find_element_by_xpath("//div[@class='pluggable-input-body copyable-text selectable-text']")
+			ele.click()
+			ele.send_keys(msg)
+			ele.send_keys(Keys.RETURN)
+		except e:
+			print(e)
+
+	def spam(self, name, interval=5):
+		msg = "Dikha"
+		try:
+			self.get_user(name)
+			ele = self.driver.find_element_by_xpath("//div[@class='pluggable-input-body copyable-text selectable-text']")
+			while True:
+				sleep(interval)
+				ele.click()
+				ele.send_keys(msg)
+				ele.send_keys(Keys.RETURN)
+		except KeyboardInterrupt as e:
+			print("Safely exited.")		
+
+# To open new tab
+# driver.execute_script('''window.open("http://bings.com","_blank");''')
